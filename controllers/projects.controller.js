44be83554a13544ed94eb18e4bb9b5e7b5c04af7
satriyarifki <rifkisatriya@gmail.com>
2024-02-07 17:05:09 +0700

    feat

--- controllers/projects.controller.js
+++ controllers/projects.controller.js
@@ -1,12 +1,13 @@
 const { Sequelize, QueryTypes } = require("sequelize");
 const { connectProduction } = require("../config/connections");
-const { projects } = require("../models");
+const { projects, departments, users_role } = require("../models");
 const Op = Sequelize.Op;
 
 exports.index = async (req, res) => {
   try {
-      
-    const response = await projects.findAll();
+    const response = await projects.findAll({
+      include: [{ model: departments }, { model: users_role }],
+    });
     // const response = await connectProduction.query("SELECT * FROM projects", {
     //   type: QueryTypes.SELECT,
     // });
--- models/projects.js
+++ models/projects.js
@@ -1,65 +1,68 @@
-const Sequelize = require('sequelize');
-module.exports = function(sequelize, DataTypes) {
-  return sequelize.define('projects', {
-    id: {
-      autoIncrement: true,
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true
+const Sequelize = require("sequelize");
+module.exports = function (sequelize, DataTypes) {
+  const Departments = sequelize.define("departments", { timestamps: false });
+  const Users = sequelize.define("users_role", { timestamps: false });
+  const Projects = sequelize.define(
+    "projects",
+    {
+      id: {
+        autoIncrement: true,
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+      },
+      departmentId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+          model: "departments",
+          key: "id",
+        },
+      },
+      usersRoleId: {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+        references: {
+          model: "users_role",
+          key: "id",
+        },
+      },
+      appName: {
+        type: DataTypes.STRING(50),
+        allowNull: false,
+        defaultValue: "",
+      },
+      progress: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        defaultValue: 0,
+      },
     },
-    deptId: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      references: {
-        model: 'departments',
-        key: 'id'
-      }
-    },
-    userRoleId: {
-      type: DataTypes.INTEGER,
-      allowNull: true,
-      references: {
-        model: 'users_role',
-        key: 'id'
-      }
-    },
-    appName: {
-      type: DataTypes.STRING(50),
-      allowNull: false,
-      defaultValue: ""
-    },
-    progress: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      defaultValue: 0
+    {
+      sequelize,
+      tableName: "projects",
+      timestamps: true,
+      indexes: [
+        {
+          name: "PRIMARY",
+          unique: true,
+          using: "BTREE",
+          fields: [{ name: "id" }],
+        },
+        {
+          name: "FK_projects_departments",
+          using: "BTREE",
+          fields: [{ name: "deptId" }],
+        },
+        {
+          name: "FK_projects_users_role",
+          using: "BTREE",
+          fields: [{ name: "userRoleId" }],
+        },
+      ],
     }
-  }, {
-    sequelize,
-    tableName: 'projects',
-    timestamps: true,
-    indexes: [
-      {
-        name: "PRIMARY",
-        unique: true,
-        using: "BTREE",
-        fields: [
-          { name: "id" },
-        ]
-      },
-      {
-        name: "FK_projects_departments",
-        using: "BTREE",
-        fields: [
-          { name: "deptId" },
-        ]
-      },
-      {
-        name: "FK_projects_users_role",
-        using: "BTREE",
-        fields: [
-          { name: "userRoleId" },
-        ]
-      },
-    ]
-  });
+  );
+  Projects.belongsTo(Departments);
+  Projects.belongsTo(Users);
+  return Projects;
 };
